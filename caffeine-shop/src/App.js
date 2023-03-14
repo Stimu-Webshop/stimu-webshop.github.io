6cac1306223eb5ccf537ef682011afb3995d2be9
n2jato00 <n2jato00@students.oamk.fi>
2023-03-14 11:59:31 +0200

    App.js body

--- caffeine-shop/src/App.js
+++ caffeine-shop/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <>
-    <h1>stimulointia</h1>
+      <NavBar />
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contactus" element={<ContactUs />} />
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
