9285c2083716e456d739a337d571d67c616f2e20
Serveriii <n2jose00@students.oamk.fi>
2023-03-14 13:19:11 +0200

    Added images and styles

--- caffeine-shop/src/pages/Home.js
+++ caffeine-shop/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Carousel from '../components/Carousel';
 
 export default function Home() {
     return (
       <div>
-        This is the home page.
+        <Carousel/>
       </div>
+
     );
   }
