512b7d9578a6cc5c01e5d7014a0fd6c37c353891
Serveriii <n2jose00@students.oamk.fi>
2023-03-14 13:31:09 +0200

    Image sizing

--- caffeine-shop/src/components/Footer.js
+++ caffeine-shop/src/components/Footer.js
@@ -3,9 +3,8 @@ import '../styles/Footer.scss'
 
 export default function Footer() {
   return (
-    <div>
-          <h1 id="footer">Footerin paikka</h1>
+    <div className='footer'>
+          <h1>Footerin paikka</h1>
     </div>
-
   )
 }
