0931d25ab9d89622b534b26c9e7b2ed7820933fd
Serveriii <n2jose00@students.oamk.fi>
2023-03-15 12:41:14 +0200

    made product page

--- caffeine-shop/src/App.js
+++ caffeine-shop/src/App.js
@@ -6,9 +6,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 // Pages
 import Home from './pages/Home';
-import About from './pages/About';
 import ContactUs from './pages/ContactUs';
 import NotFound from './pages/NotFound';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
           <Route path="contactus" element={<ContactUs />} />
           <Route path="*" element={<NotFound />} /> 
         </Routes>
--- caffeine-shop/src/pages/Products.js
+++ caffeine-shop/src/pages/Products.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function About() {
+export default function Products() {
   return (
     <div>
         lorem ipsum
