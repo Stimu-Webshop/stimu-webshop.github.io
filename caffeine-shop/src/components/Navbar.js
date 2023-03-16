2153d35d1fb960351380227ce8fffa822dd098c1
Serveriii <n2jose00@students.oamk.fi>
2023-03-16 13:33:55 +0200

    Removed "Home" button

--- caffeine-shop/src/components/Navbar.js
+++ caffeine-shop/src/components/Navbar.js
@@ -26,13 +26,8 @@ export default function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
                             <Link className="nav-link" to="/products">
-                                Products
+                                Tuotteet
                             </Link>
                         </li>
                         <li className="nav-item">
