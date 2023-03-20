52d6d06e659ecb6617dc1f728e1f1b4c134895bc
n2jato00 <n2jato00@students.oamk.fi>
2023-03-20 15:15:13 +0200

    delete button and add close to div

--- caffeine-shop/src/pages/ProductPage.js
+++ caffeine-shop/src/pages/ProductPage.js
@@ -5,12 +5,11 @@ function ProductPage(props) {
   const { product, onClose } = props;
 
   return (
-    <div className="product-page">
+    <div className="product-page" onClick={onClose}>
       <h2>{product.name}</h2>
       <img src={product.img} alt={product.name} />
       <p>{product.description}</p>
       <p>{product.price} eur</p>
-      <button onClick={onClose}>Close</button>
     </div>
   );
 }
