8a1874fa5457422cf9e0d99e4c74dbfd148fc5a5
Serveriii <n2jose00@students.oamk.fi>
2023-03-20 15:55:30 +0200

    Functionality for category pages

--- caffeine-shop/src/pages/Coffee.js
+++ caffeine-shop/src/pages/Coffee.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import ProductPage from "./ProductPage";
+
 
 export default function Coffee() {
+    const [searchResults, setSearchResults] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [selectedItem, setSelectedItem] = useState(null);
 
-    return (
-        <div>
-            <h1>Kahvia saatana</h1>
-        </div>
-    )
-}
+
+    useEffect(() => {
+        const PHP = "http://localhost:3001/products/categories.php?category=2";
+        axios.get(PHP)
+          .then(response => {
+            setError(null);
+            setIsLoaded(true);
+            setSearchResults(response.data);
+            console.log(response.data)
+          })
+          .catch(error => {
+            alert(error);
+          });
+      }, []);
+
+      function handleProductClick(product) {
+        setSelectedItem(product);
+      }
+
+      function handleClose() {
+        setSelectedItem(null);
+      }
+
+      if (selectedItem) {
+        return (
+          <ProductPage product={selectedItem} onClose={handleClose} />
+        );
+      } else if (error) {
+        return <p>{error.message}</p>;
+      } else if (!isLoaded) {
+        return <p>Loading...</p>;
+      } else {
+        return (
+          <div>
+            <h1>Tuotteet</h1>
+            <div className='product-container'>
+              {searchResults.map((product) => (
+                <div className="product" key={product.id} onClick={() =>
+                  handleProductClick(product)}>
+                  <img src={product.img} alt="" srcSet="" />
+                  <ul>
+                    <h5>{product.name}</h5>
+                    <li>{product.description.substring(0, 100)}{product.description.length > 100 ? "..." : ""}</li>
+                    <li>{product.price} eur</li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      }
+    }
+
--- caffeine-shop/src/pages/EnergyDrinks.js
+++ caffeine-shop/src/pages/EnergyDrinks.js
@@ -1,11 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ProductPage from "./ProductPage";
+
 
 export default function EnergyDrinks() {
-    return (
-        <div>
-        <h1>Energy Drinks</h1>
-        <Link to="/">Home</Link>
-        </div>
-    );
+    const [searchResults, setSearchResults] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+
+    useEffect(() => {
+        const PHP = "http://localhost:3001/products/categories.php?category=1";
+        axios.get(PHP)
+          .then(response => {
+            setError(null);
+            setIsLoaded(true);
+            setSearchResults(response.data);
+            console.log(response.data)
+          })
+          .catch(error => {
+            alert(error);
+          });
+      }, []);
+
+      function handleProductClick(product) {
+        setSelectedItem(product);
+      }
+
+      function handleClose() {
+        setSelectedItem(null);
+      }
+
+      if (selectedItem) {
+        return (
+          <ProductPage product={selectedItem} onClose={handleClose} />
+        );
+      } else if (error) {
+        return <p>{error.message}</p>;
+      } else if (!isLoaded) {
+        return <p>Loading...</p>;
+      } else {
+        return (
+          <div>
+            <h1>Tuotteet</h1>
+            <div className='product-container'>
+              {searchResults.map((product) => (
+                <div className="product" key={product.id} onClick={() =>
+                  handleProductClick(product)}>
+                  <img src={product.img} alt="" srcSet="" />
+                  <ul>
+                    <h5>{product.name}</h5>
+                    <li>{product.description.substring(0, 100)}{product.description.length > 100 ? "..." : ""}</li>
+                    <li>{product.price} eur</li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      }
     }
