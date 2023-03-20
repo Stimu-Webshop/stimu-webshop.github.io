e635760e3d57287bd9000b3a27ecd65697903943
n2jato00 <n2jato00@students.oamk.fi>
2023-03-20 15:15:51 +0200

    add button again

--- caffeine-shop/src/pages/ProductPage.js
+++ caffeine-shop/src/pages/ProductPage.js
@@ -5,11 +5,12 @@ function ProductPage(props) {
   const { product, onClose } = props;
 
   return (
-    <div className="product-page" onClick={onClose}>
+    <div className="product-page">
       <h2>{product.name}</h2>
       <img src={product.img} alt={product.name} />
       <p>{product.description}</p>
       <p>{product.price} eur</p>
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
