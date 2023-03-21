19a4393fc0a11a31308973cb4f9669c231516c01
sajosu <saritu00@hotmail.com>
2023-03-21 15:03:36 +0200

    updated product diary and added cartpage

---
+++ caffeine-shop/src/pages/CartPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ShoppingCart from '../components/Shoppingcart'
+
+export default function CartPage() {
+  return (
+    <div>
+        <ShoppingCart />
+    </div>
+  )
+}
