f8d499086683458dcab699f6b0c437bfca31fa8d
sajosu <saritu00@hotmail.com>
2023-03-22 13:01:19 +0200

    Ui elements for productPage

--- caffeine-shop/src/components/CartContent.js
+++ caffeine-shop/src/components/CartContent.js
@@ -1,26 +1,43 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
-export default function CartContent()    {
+export default function CartContent() {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    useEffect(() => {
+        const PHP = "https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/shopping_cart/getcart.php";
+        axios.get(PHP)
+            .then(response => {
+                setError(null);
+                setIsLoaded(true);
+                setCartItems(response.data);
+            })
+            .catch(error => {
+                setError(error);
+            });
+    }, []);
+
     return (
         <>
-        <div className="cartItem">
-  <div className="left">
-    <ul>
-      <li>Tuote</li>
-      <li>Määrä: x</li>
-      <li>Summa: y</li>
-    </ul>
-  </div>
-  <div className="right">
-    <img src="https://via.placeholder.com/300x300" />
-  </div>
-</div>
-<p>Yhteensä: 94 589 eur</p>
-    
-    
-
-    </>
+            <div className="cartItem">
+                <div className="left">
+                    <ul>
+                        <li>Tuote</li>
+                        <li>Määrä: x</li>
+                        <li>Summa: y</li>
+                    </ul>
+                </div>
+                <div className="right">
+                    <img src="https://via.placeholder.com/300x300" />
+                </div>
+            </div>
+            <p>Yhteensä: 94 589 eur</p>
+        </>
     )
 }
--- caffeine-shop/src/pages/ProductPage.js
+++ caffeine-shop/src/pages/ProductPage.js
@@ -3,12 +3,21 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import '../styles/ProductPage.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faXmark, faStar } from "@fortawesome/free-solid-svg-icons";
 
 const ProductPage = () => {
   const { id } = useParams();
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [product, setProduct] = useState(null);
+  const [amount, setAmount] = useState(0);
+
+  const buyAmount = []
+
+  for (let i = 0; i < 11; i++) {
+    buyAmount.push(i)
+  }
 
   useEffect(() => {
     const PHP = `https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/selected.php?id=${id}`;
@@ -29,15 +38,50 @@ const ProductPage = () => {
     return <p>Loading...</p>;
   } else {
     return (
-      <div>
+
+      <div className='productContainer'>
         <h1>{product.name}</h1>
+        <div className='productPic'>
         <img src={product.img} alt="" srcSet="" />
+        </div>
+        <div className='productInfo'>
         <ul>
           <li>{product.description}</li>
-          <li>{product.price} eur</li>
+          <li>Varastossa: {product.inventory_id} 
+          <FontAwesomeIcon
+              icon={faCheck}
+              className='check'
+              /> 
+          <FontAwesomeIcon
+              icon={faXmark} 
+              className='xmark'
+              /> 
+              </li>
+          <li>Hinta: {product.price} eur</li>
         </ul>
+        <label>Määrä: </label>
+        <select name="amount" onChange={e => setAmount(e.target.value)}>
+          {buyAmount.map(value => <option value={value}>{value}</option>)}
+          </select>
+
+        <button onClick={ () => console.log(amount)}>Lisää ostoskoriin</button>
+        {/* On click funktio jätetty, jos tarvii vielä myöhemmin tarkastaa määrää */}
+        </div>
+        
         <Link className='backLink' to="/products">Back</Link>
+        
+          <h3>Arvostelut</h3>
+        <div className='rating'>
+          <div className='ratingStars'>
+            <FontAwesomeIcon icon={faStar} className='star' id="star1" />
+            <FontAwesomeIcon icon={faStar} className='star' id="star2" />
+            <FontAwesomeIcon icon={faStar} className='star' id="star3" />
+            <FontAwesomeIcon icon={faStar} className='star' id="star4" />
+            <FontAwesomeIcon icon={faStar} className='star' id="star5" />
+            </div>
+        </div>
       </div>
+      
     );
   }
 };
