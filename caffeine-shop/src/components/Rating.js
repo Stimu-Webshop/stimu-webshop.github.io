e4444022bd7187393f9a4cc9bc55cc734910586f
Serveriii <n2jose00@students.oamk.fi>
2023-03-23 13:05:03 +0200

    Styling for review section

--- caffeine-shop/src/components/Rating.js
+++ caffeine-shop/src/components/Rating.js
@@ -26,6 +26,14 @@ export default function Rating() {
         <div className="review">
             <h3>{name}</h3>
             <p>{comment}</p>
+            <div className='ratingStars'>
+                    {/* Tässäkin on nyt vaan nämä ikonit, pitää lisätä vielä input kenttiä ja hover toiminnallisuudet (rating ja värin vaihtuminen) ikoneille */}
+                    <FontAwesomeIcon icon={faStar} className='star' id="star1" />
+                    <FontAwesomeIcon icon={faStar} className='star' id="star2" />
+                    <FontAwesomeIcon icon={faStar} className='star' id="star3" />
+                    <FontAwesomeIcon icon={faStar} className='star' id="star4" />
+                    <FontAwesomeIcon icon={faStar} className='star' id="star5" />
+                </div>
         </div>
     );
 
@@ -35,26 +43,18 @@ export default function Rating() {
     return (
         <>
             <h3>Arvostelut</h3>
-            <div className='rating'>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className="review-form">
                     <label>Nimi:</label>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
                     <label>Arvostelu:</label>
                     <textarea name="review" rows="4" cols="33" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
                     <button type="submit" className="btn btn-primary" onClick={reviewHandler}>Lähetä</button>
                     {/* Tilamuuttujaan napilla syötetyt tiedot annetaan arvostelukomponentille ja kutsutaan komponentti */}
-                    {review.map((review, index) => (
+                </form>
+                <div className='rating'>
+                {review.map((review, index) => (
                         <Review key={index} name={review.name} comment={review.comment} />
                     ))}
-                </form>
-                <div className='ratingStars'>
-                    {/* Tässäkin on nyt vaan nämä ikonit, pitää lisätä vielä input kenttiä ja hover toiminnallisuudet (rating ja värin vaihtuminen) ikoneille */}
-                    <FontAwesomeIcon icon={faStar} className='star' id="star1" />
-                    <FontAwesomeIcon icon={faStar} className='star' id="star2" />
-                    <FontAwesomeIcon icon={faStar} className='star' id="star3" />
-                    <FontAwesomeIcon icon={faStar} className='star' id="star4" />
-                    <FontAwesomeIcon icon={faStar} className='star' id="star5" />
-                </div>
             </div>
         </>
     )
