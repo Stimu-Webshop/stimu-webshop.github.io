0f15e88f779ef57e7a574c76dc47c966e51e377e
Serveriii <n2jose00@students.oamk.fi>
2023-03-23 12:32:43 +0200

    Customer review component added

--- caffeine-shop/src/components/Rating.js
+++ caffeine-shop/src/components/Rating.js
@@ -1,17 +1,51 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
-import Reviews from "./ReviewTEST";
 
 
 
+// TÄÄ TARVII TYYLITTELYÄ T. SEPI
 
 export default function Rating() {
+    const [name, setName] = useState('');
+    const [comment, setComment] = useState('');
+    const [review, setReview] = useState([]);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setName("");
+        setComment("");
+    };
+
+    const reviewHandler = () => {
+        setReview([...review, { name: name, comment: comment }]);
+    };
+
+    const Review = ({ name, comment }) => (
+        <div className="review">
+            <h3>{name}</h3>
+            <p>{comment}</p>
+        </div>
+    );
+
+
+
 
     return (
         <>
             <h3>Arvostelut</h3>
             <div className='rating'>
+                <form onSubmit={handleSubmit}>
+                    <label>Nimi:</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    <label>Arvostelu:</label>
+                    <textarea name="review" rows="4" cols="33" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                    <button type="submit" className="btn btn-primary" onClick={reviewHandler}>Lähetä</button>
+                    {review.map((review, index) => (
+                        <Review key={index} name={review.name} comment={review.comment} />
+                    ))}
+                </form>
                 <div className='ratingStars'>
                     {/* Tässäkin on nyt vaan nämä ikonit, pitää lisätä vielä input kenttiä ja hover toiminnallisuudet (rating ja värin vaihtuminen) ikoneille */}
                     <FontAwesomeIcon icon={faStar} className='star' id="star1" />
