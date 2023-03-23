0616bdde93b87eb96d889a4191301c18d8ea1373
sajosu <saritu00@hotmail.com>
2023-03-23 08:50:55 +0200

    few comments and functionalities

--- caffeine-shop/src/pages/ProductPage.js
+++ caffeine-shop/src/pages/ProductPage.js
@@ -1,95 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import '../styles/ProductPage.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faXmark, faStar } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import '../styles/ProductPage.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheck, faXmark, faStar } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom'
 
 const ProductPage = () => {
-  const { id } = useParams();
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [product, setProduct] = useState(null);
-  const [amount, setAmount] = useState(0);
+  const { id } = useParams()
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [product, setProduct] = useState(null)
+  const [amount, setAmount] = useState(0)
 
   const buyAmount = []
 
   for (let i = 0; i < 11; i++) {
     buyAmount.push(i)
   }
-  const navigate = useNavigate();
-
-
+  const navigate = useNavigate()
 
   useEffect(() => {
     const PHP = `https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/selected.php?id=${id}`;
     axios.get(PHP)
       .then(response => {
-        setError(null);
-        setIsLoaded(true);
-        setProduct(response.data[0]);
+        setError(null)
+        setIsLoaded(true)
+        setProduct(response.data[0])
       })
       .catch(error => {
-        setError(error);
-      });
-  }, [id]);
-
-
-    
+        setError(error)
+      })
+  }, [id])
 
   if (error) {
-    return <p>{error.message}</p>;
+    return <p>{error.message}</p>
   } else if (!isLoaded) {
-    return <p>Loading...</p>;
+    return <p>Loading...</p>
   } else {
     return (
 
       <div className='productContainer'>
         <h1>{product.name}</h1>
         <div className='productPic'>
-        <img src={product.img} alt="" srcSet="" />
+          <img src={product.img} alt="" srcSet="" />
         </div>
         <div className='productInfo'>
-        <ul>
-          <li>{product.description}</li>
-          <li>Varastossa: {product.inventory_id} 
-          <FontAwesomeIcon
-              icon={faCheck}
-              className='check'
-              /> 
-          <FontAwesomeIcon
-              icon={faXmark} 
-              className='xmark'
-              /> 
-              </li>
-          <li>Hinta: {product.price} eur</li>
-        </ul>
-        <label>Määrä: </label>
-        <select name="amount" onChange={e => setAmount(e.target.value)}>
-          {buyAmount.map(value => <option value={value}>{value}</option>)}
+          <ul>
+            <li>{product.description}</li>
+            {/* Jos varastossa -> checkmark, muuten rasti */}
+            <li>Varastossa: {product.quantity}
+              {product.quantity > 0 ?
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className='check' /> :
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  className='xmark' />}
+            </li>
+            <li>Hinta: {product.price} eur</li>
+          </ul>
+          <label>Määrä: </label>
+          <select name="amount" onChange={e => setAmount(e.target.value)}>
+            {buyAmount.map(value => <option value={value}>{value}</option>)}
           </select>
 
-        <button onClick={ () => console.log(amount)}>Lisää ostoskoriin</button>
-        {/* On click funktio jätetty, jos tarvii vielä myöhemmin tarkastaa määrää */}
+          <button onClick={() => console.log(amount)}>Lisää ostoskoriin</button>
+          {/* On click funktio jätetty, jos tarvii vielä myöhemmin tarkastaa määrää */}
         </div>
-        
+
         <button onClick={() => navigate(-1)}>Go back</button>
-        
-          <h3>Arvostelut</h3>
+
+        <h3>Arvostelut</h3>
         <div className='rating'>
           <div className='ratingStars'>
+            {/* Tässäkin on nyt vaan nämä ikonit, pitää lisätä vielä input kenttiä ja hover toiminnallisuudet (rating ja värin vaihtuminen) ikoneille */}
             <FontAwesomeIcon icon={faStar} className='star' id="star1" />
             <FontAwesomeIcon icon={faStar} className='star' id="star2" />
             <FontAwesomeIcon icon={faStar} className='star' id="star3" />
             <FontAwesomeIcon icon={faStar} className='star' id="star4" />
             <FontAwesomeIcon icon={faStar} className='star' id="star5" />
-            </div>
+          </div>
         </div>
       </div>
-      
-    );
-  }
-};
 
-export default ProductPage;
+    )
+  }
+}
+
+export default ProductPage
