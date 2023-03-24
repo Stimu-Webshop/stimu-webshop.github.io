3e58b1a7a856cce4e0e8daa57fac2a4d1d431a48
n2jato00 <n2jato00@students.oamk.fi>
2023-03-24 11:14:33 +0200

    change alert text and user_id is now 1

--- caffeine-shop/src/pages/CartPage.js
+++ caffeine-shop/src/pages/CartPage.js
@@ -6,12 +6,9 @@ import { useState } from "react";
 
 export default function CartPage() {
 
-  const [error, setError] = useState(null);
   const [isOrdering, setIsOrdering] = useState(false);
-  const [message, setMessage] = useState(null);
   const orderData = {
-    user_id: 2, // replace with the actual user ID
-    // other order data...
+    user_id: 1, // tällä hetkellä tilaukset menee aina käyttäjälle 1
   };
 
   const handlePlaceOrder = () => {
@@ -19,39 +16,39 @@ export default function CartPage() {
 
     axios
       .post(
-        "https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/deletecart.php",
+        "https://www.students.oamk.fi/~n2jato00/PHP/products/deletecart.php",
         orderData
       )
-      .then((response) => {
-        setIsOrdering(false);
-        setMessage(response.data.message);
+      .then(() => {
+        // Order successful
+        alert('Order successful');
+        // Clear cart or update cart as needed
       })
-      .catch((error) => {
-        setIsOrdering(false);
-        setError(error);
+      .catch(() => {
+        // Order failed
+        alert('Order failed');
       });
   };
 
   const handleDeleteCart = () => {
-    axios.post("https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/shopping_cart/deletecart.php",
+    axios.post("https://www.students.oamk.fi/~n2jato00/PHP/products/deletecart.php",
       orderData
       )
-      .then(response => {
-        alert("Cart deleted successfully");
-        // Clear cart
+      .then(() => {
+        // Delete successful
+        alert('Delete successful');
+        // Update product list or do other actions as needed
       })
-      .catch(error => {
-        console.log(error);
-        alert("Error deleting cart");
+      .catch(() => {
+        // Delete failed
+        alert('Delete failed');
       });
-  }
+  };
   return (
     <div className='cartpage'>
       <h2>Ostoskori</h2>
       <CartContent />
       <button onClick={handlePlaceOrder} disabled={isOrdering}>Vahvista ostokset</button>
-      {message && <p>{message}</p>}
-      {error && <p>Error: {error.message}</p>}
       <button className='emptyCart' onClick={handleDeleteCart}>Tyhjennä kori</button>
     </div>
   )
