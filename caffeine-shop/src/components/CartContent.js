c5cea09a83ea0347e9d1201e0e77636e9c03b638
sajosu <saritu00@hotmail.com>
2023-03-30 14:38:44 +0300

    deletecartitem

--- caffeine-shop/src/components/CartContent.js
+++ caffeine-shop/src/components/CartContent.js
@@ -7,8 +7,12 @@ export default function CartContent() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [productId, setProductId] = useState([]);
+  
   const storedUserId = JSON.parse(localStorage.getItem('userId'));
   const userId = storedUserId ? storedUserId.userId : null;
+
+
   
   // 17:09 28.3.23 CART FUNKTIOT TOIMII VAIN SISÄÄNKIRJAUTUNEENA.
   // TÄHÄN RATKAISUA LÄHITULEVAISUUDESSA
@@ -23,12 +27,28 @@ export default function CartContent() {
         setError(null);
         setIsLoaded(true);
         setCartItems(response.data);
+        setProductId(response.data.product_id)
       })
       .catch((error) => {
         setError(error);
       });
   }, [cartItems, userId]);
- 
+
+  //Delete item from shopping
+  const deleteCartItem = () => {
+    const url = "https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/deleteitem.php"
+    const deleteData = { user_id: userId, product_id: productId };
+    axios
+      .post(url, {deleteData} )
+      .then((response) => {
+        console.log(response)
+        // Refresh the cart items after deletion
+        setCartItems([]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const totalPrice = cartItems.reduce((acc, item) => acc + parseFloat(item.total), 0);
 
@@ -48,7 +68,7 @@ export default function CartContent() {
                 <li className="itemName">{item.name}</li>
                 <li>Määrä: {item.quantity}</li>
                 <li>Summa: {item.total} eur</li>
-                <li className="deleteItem"><FontAwesomeIcon
+                <li className="deleteItem" onClick={() => deleteCartItem()}><FontAwesomeIcon
             icon={faTrashCan}/> Poista tuote</li>
               </ul>
             </div>
