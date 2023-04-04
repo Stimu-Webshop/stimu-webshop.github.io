33afcb109dd028539a0f557ecbf744ff8b01b48b
Serveriii <n2jose00@students.oamk.fi>
2023-04-04 08:22:51 +0300

    added a cat

--- caffeine-shop/src/pages/NotFound.js
+++ caffeine-shop/src/pages/NotFound.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import catimg from "../img/404-kissa.png"
+import "../styles/NotFound.scss"
 
 export default function NotFound() {
   return (
+    <>
     <div class="notfound">404</div>
+    <img src={catimg} alt="cat" />
+    </>
   )
 }
