d6b61ae2caa37e80d65fb78f738dd269a9dd0420
Serveriii <n2jose00@students.oamk.fi>
2023-04-10 19:12:16 +0300

    Footer styles

--- caffeine-shop/src/components/Footer.js
+++ caffeine-shop/src/components/Footer.js
@@ -7,7 +7,17 @@ export default function Footer() {
     <footer>
         <Link className='footerLink' to="/contactus">Palaute</Link>
           
-          <p>Toinen laatikko missä lukee propagandaa huumeiden käytöstä</p>
+          <p>
+            Stimu.fi on TIK22SP Study Group 1 Ryhmä A:n yhteistyöprojekti web-palveluiden projektikurssille. <br />
+            Aiheeksi valitsimme jokaiselle koodarille tutut kofeiini- ja piristetuotteet. <br />
+            Stimu.fi on toteutettu Reactilla, PHP:lla ja MySQL-tietokannalla, taustatukenaan Bootstrap ja SASS. <br />
+            <br />
+            Työryhmä:<br />
+            Severi Jokelainen <br />
+            Sari Suomela<br />
+            Tommi Järvenpää<br />
+            Samuli Ruotsalainen<br />
+          </p>
     </footer>
   )
 }
