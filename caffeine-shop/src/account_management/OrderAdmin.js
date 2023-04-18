9bf385654ed60926a1981c9f5638b6c355e5b6a8
n2jato00 <n2jato00@students.oamk.fi>
2023-04-18 12:51:33 +0300

    not work

--- caffeine-shop/src/account_management/OrderAdmin.js
+++ caffeine-shop/src/account_management/OrderAdmin.js
@@ -3,17 +3,24 @@ import axios from 'axios'
 import '../styles/Admin.scss'
 
 export default function OrderAdmin() {
+  const [orders, setOrders] = useState([]);
 
-        const [orders, setOrders] = useState([]);
-      
-        useEffect(() => {
-          axios.get('https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/admin_getorders.php')
-            .then(response => setOrders(response.data))
-            .catch(error => console.log(error));
-        }, []);
+  useEffect(() => {
+    axios.post('https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/admin_getorders.php')
+      .then(response => setOrders(response.data))
+      .catch(error => console.log(error));
+  }, []);
 
-    return (
-        <table className='order-table'>
+  const handleOrderDelivered = (orderId) => {
+    axios.post('https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/admin_getorders.php', {
+      id: orderId,
+    })
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
+
+  return (
+    <table className='order-table'>
       <thead>
         <tr>
           <th>Order ID</th>
@@ -29,28 +36,22 @@ export default function OrderAdmin() {
         {orders.map(order => (
           <tr key={order.row_id} className='order-table-column'>
             <td>{order.row_id}</td>
+            <td><p>{order.order_date}</p></td>
+            <td><p>{order.user_id}</p></td>
+            <td><p>{order.ordered_product_id}</p></td>
+            <td><p>{order.product_quantity}</p></td>
+            <td><p>{order.delivered}</p></td>
             <td>
-              <p>{order.order_date}</p> 
-            </td>
-            <td>
-              <p>{order.user_id}</p>
-            </td>
-            <td>
-              <p>{order.ordered_product_id}</p>
-            </td>
-            <td>
-              <p>{order.product_quantity}</p>
-            </td>
-            <td>
-              <p>{order.delivered}</p>
-            </td>
-            <td>
-              <button className="btn btn-success">Toimitettu</button>
+              <button
+                className="btn btn-success"
+                onClick={() => handleOrderDelivered(order.row_id)}
+              >
+                Toimitettu
+              </button>
             </td>
           </tr>
-
         ))}
       </tbody>
     </table>
   );
-        }
+}
