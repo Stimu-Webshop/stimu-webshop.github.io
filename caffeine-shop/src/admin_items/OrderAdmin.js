aa3bed7229d15dd4b820c2ba1e922f76b7b910b3
saruo <ruotsalainen.samuli@gmail.com>
2023-04-20 08:53:13 +0300

    Updated orderadmin page to allow multiple updates

--- caffeine-shop/src/admin_items/OrderAdmin.js
+++ caffeine-shop/src/admin_items/OrderAdmin.js
@@ -6,6 +6,8 @@ import '../styles/Admin.scss'
 export default function OrderAdmin() {
 
   const [isLoading, setIsLoading] = useState(true)
+  const [selectedOrders, setSelectedOrders] = useState([]);
+
 
   useEffect(() => {
     const adminValue = localStorage.getItem('adminValue')
@@ -25,13 +27,27 @@ export default function OrderAdmin() {
       .catch(error => console.log(error));
   }, [orders]);
 
-  const handleOrderDelivered = (orderId) => {
-    const id = orderId
+  const handleOrderDelivered = () => {
+    const confirmed = window.confirm('Merkataanko valitut tuotteet toimitetuiksi?');
+    if (!confirmed) {
+      return
+    }
+    
+    const ids = selectedOrders
     axios.post('https://www.students.oamk.fi/~n2rusa00/Stimu/backendi/Web-Shop-Back/products/admin_updateorders.php',
-      id)
-      .then(response => console.log(response))
+      ids)
+      .then(response => console.log(response) )
       .catch(error => console.log(error));
   }
+
+  const handleCheckboxChange = (event, orderId) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      setSelectedOrders([...selectedOrders, orderId]);
+    } else {
+      setSelectedOrders(selectedOrders.filter(id => id !== orderId));
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -47,7 +63,12 @@ export default function OrderAdmin() {
             <th>Product ID</th>
             <th>Quantity</th>
             <th>Delivered</th>
-            <th>Action</th>
+            <th>    <button
+                  className="btn btn-success"
+                  onClick={() => handleOrderDelivered()}
+                >
+                  Merkkaa toimitetuksi
+                </button></th>
           </tr>
         </thead>
         <tbody>
@@ -60,12 +81,7 @@ export default function OrderAdmin() {
               <td><p>{order.product_quantity}</p></td>
               <td><p>{order.delivered}</p></td>
               <td>
-                <button
-                  className="btn btn-success"
-                  onClick={() => handleOrderDelivered(order.row_id)}
-                >
-                  Toimitettu
-                </button>
+              <input id='set-as-delivered' type='checkbox' onChange={(event) => handleCheckboxChange(event, order.row_id)}></input>    
               </td>
             </tr>
           ))}
