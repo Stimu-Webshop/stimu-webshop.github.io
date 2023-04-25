9128bc2b2cef432559885cb8beb7913a22030bdf
Serveriii <n2jose00@students.oamk.fi>
2023-04-25 15:20:58 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Web-Shop-Front
-
-This is the repository for our web shop projects front end code.
-
-Study Group 1-A
--- caffeine-shop/src/account_management/RegisteryPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import RegisteryFunction from './RegisteryFunction'
-import '../styles/Registry.scss'
-
-export default function RegisteryPage() {
-  return (
-    <RegisteryFunction/>
-  )
-}
